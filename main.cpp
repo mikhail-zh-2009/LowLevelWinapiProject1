aba3faa8166c910d2f19b3399279ab1682a5e684
Mihail Zhizhikin <mihailzizikin330@gmail.com>
2023-09-23 18:02:16 +0500

    program closes itself when window closed

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <windows.h>
 
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	if (msg == WM_DESTROY) {
+		PostQuitMessage(0);
+	}
+
 	return DefWindowProcW(hWnd, msg, wParam, lParam);
 }
 
@@ -20,6 +24,7 @@ int main() {
 		if (PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE)) {
 			TranslateMessage(&msg);
 			DispatchMessageW(&msg);
+			if (msg.message == WM_QUIT) break;
 		}
 	}
 
